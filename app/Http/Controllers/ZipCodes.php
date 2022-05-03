140ca2e19d04cc8a3ddb22032b0d252c62eb0c98
Frank Reyes <freyesm91@gmail.com>
2022-05-03 13:04:50 -0500

    Add validations Add status to response Add exceptions Format of the strings

--- app/Http/Controllers/ZipCodes.php
+++ app/Http/Controllers/ZipCodes.php
@@ -6,31 +6,60 @@ use Illuminate\Http\Request;
 
 class ZipCodes extends Controller
 {
-    public function show ( $zip_code ){
-        $zip_codes = json_decode(file_get_contents(storage_path()."\json\zip_codes.json"), true);
-        $zip_codes = array_filter($zip_codes);
-        $zip_codes = collect($zip_codes)->where("d_codigo", "LIKE", "$zip_code")->all();
+    public function show ( $zip_code ) {
 
-        $data = array(
-            "zip_code" => '',
-            "locality" => '',
-            "federal_entity" => array(
-                "key" => '',
-                "name" => '',
-                "code" => ''
-            ),
-            "settlements" => array(
-                array(
-                    "key" => '',
-                    "name" => '',
-                    "code" => ''
-                )
-            ),
-            "municipality" => array(
-                "key" => '',
-                "name" => ''
-            ),
-        );
-        return response()->json(array( 'data' => $data ) );
+        try {
+            setlocale(LC_ALL, 'en_US.utf8');
+
+            $table = array(
+                'Š'=>'S', 'š'=>'s', 'Đ'=>'Dj', 'đ'=>'dj', 'Ž'=>'Z', 'ž'=>'z', 'Č'=>'C', 'č'=>'c', 'Ć'=>'C', 'ć'=>'c',
+                'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+                'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O',
+                'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss',
+                'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e',
+                'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o',
+                'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b',
+                'ÿ'=>'y', 'Ŕ'=>'R', 'ŕ'=>'r',
+            );
+
+            $zip_codes   = json_decode( file_get_contents( storage_path()."\json\zip_codes.json" ), true );
+            $zip_codes   = array_filter($zip_codes);
+            $data        = collect( $zip_codes )->where( "d_codigo", "=", "$zip_code" )->values( )->all( );
+            $settlements = array();
+
+            foreach( $data as $d ){
+                $settlement = array(
+                    "key"             => intval( $d['id_asenta_cpcons'] ),
+                    "name"            => strtoupper( $d['d_asenta'] ),
+                    "zone_type"       => strtoupper( $d['d_zona'] ),
+                    "settlement_type" => array(
+                        "name" => $d[ 'd_tipo_asenta' ],
+                    )
+                );
+                array_push( $settlements, $settlement );
+            }
+
+            $response = array(
+                "zip_code"       => $data[ 0 ][ 'd_codigo' ] ?? '',
+                "locality"       => strtoupper( strtr( $data[ 0 ][ 'd_ciudad' ], $table ) ) ?? '',
+                "federal_entity" => array(
+                    "key"  => intval( $data[ 0 ][ 'c_estado' ] ) ?? '',
+                    "name" => strtoupper( strtr( $data[ 0 ][ 'd_estado' ], $table ) ) ?? '',
+                    "code" => null
+                ),
+                "settlements"  => $settlements,
+                "municipality" => array(
+                    "key"  => intval( $data[ 0 ][ 'c_mnpio' ] ) ?? '',
+                    "name" => strtoupper( strtr( $data[ 0 ][ 'D_mnpio' ], $table ) ) ?? ''
+                ),
+            );
+
+            return response( )->json(  $response, 200 );
+
+        }catch (\Exception $e) {
+
+            return response( )->json( array( 'code'=>$e->getCode(), 'data'=>$e->getMessage() ),400 );
+
+        }
     }
 }
